c2a293d698e7d1cb264496e6ea24cdb7135cc87a
HuangYijie <h1886@outlook.com>
2015-12-08 00:30:38 +0800

    parse wikia special items html source

--- src/main/java/org/cnhalo/script/WikiaDataParse.java
+++ src/main/java/org/cnhalo/script/WikiaDataParse.java
@@ -23,67 +23,59 @@ import org.jsoup.select.Elements;
  */
 public class WikiaDataParse {
 
-	public static void main(String[] args) throws Exception {
-		
-		// factory materials
-		genFactoryMaterial();
-		
-		// commercial materials
-		Map<Integer, String> storeId2File = new LinkedHashMap<Integer, String>() {
-			private static final long serialVersionUID = 5871173947613453321L;
-			{
-				put(1, "build_supplies_store.html");
-				put(2, "hardware_store.html");
-				put(3, "farmers_market.html");
-				put(4, "furniture_store.html");
-				put(5, "gardening_supplies.html");
-				put(6, "donut_shop.html");
-				put(7, "fashion_store.html");
-				put(8, "fast_food_restaurant.html");
-				put(9, "home_appliances.html");
-			}
-		};
-		
-		for(Map.Entry<Integer, String> entry : storeId2File.entrySet()) {
-			System.out.println("/**");
-			System.out.println(" * " + entry.getValue().replace(".html", ""));
-			System.out.println(" */ ");
-			System.out.println(genCommercialMaterial(entry.getKey(), entry.getValue()));
-			System.out.println();
+	private static final Map<Integer, String> STOREID_2_FILE = new LinkedHashMap<Integer, String>() {
+		private static final long serialVersionUID = 5871173947613453321L;
+		{
+			put(1, "build_supplies_store.html");
+			put(2, "hardware_store.html");
+			put(3, "farmers_market.html");
+			put(4, "furniture_store.html");
+			put(5, "gardening_supplies.html");
+			put(6, "donut_shop.html");
+			put(7, "fashion_store.html");
+			put(8, "fast_food_restaurant.html");
+			put(9, "home_appliances.html");
 		}
-		
+	};
+
+	public static void main(String[] args) throws Exception {
+
+		// factory materials
+		// genFactoryMaterial();
+
+		// commercial materials
+		// genCommercialMaterial();
+
 		// TODO special materials
-		
+		genSpecialItems();
 	}
-	
-	public static String genCommercialMaterial(int storeId, String fileName) throws Exception {
 
-		File file = new File(PathUtil.getFileWorkPath("wikia", fileName));
-		Document document = Jsoup.parse(file, "GBK");
-		Elements ths = document.select("html body table thead tr th");
-		Map<String, Object> cacheTemplate = new LinkedHashMap<>();
-		ths.forEach(new Consumer<Element>() {
-			@Override
-			public void accept(Element t) {
-				cacheTemplate.put(getThText(t), "");
-			}
-		});
-		
-		Elements trsInTBody = document.select("html body table tbody tr");
-		
-		StringBuilder ret = new StringBuilder();
-		trsInTBody.forEach(new Consumer<Element>() {
+	public static void genCommercialMaterial() {
 
-			@Override
-			public void accept(Element tr) {
-				Map<String, Object> cache = new LinkedHashMap<>(cacheTemplate);
-				String id = "";
-				for (int j = 0; j < tr.children().size(); j++) {
-					List<String> keyList = new ArrayList<String>(cache.keySet());
-					Element td = tr.child(j);
-					String text = "";
-					if (j == 0) {
-						// 第一列有图标，处理
+		STOREID_2_FILE.forEach((storeId, fileName) -> {
+
+			try {
+				File file = new File(PathUtil.getFileWorkPath("doc", "wikia", fileName));
+				Document document = Jsoup.parse(file, "GBK");
+				Elements ths = document.select("html body table thead tr th");
+				Map<String, Object> cacheTemplate = new LinkedHashMap<>();
+				ths.forEach(t -> {
+					cacheTemplate.put(getThText(t), "");
+				});
+
+				Elements trsInTBody = document.select("html body table tbody tr");
+
+				StringBuilder ret = new StringBuilder();
+
+				trsInTBody.forEach(tr -> {
+					Map<String, Object> cache = new LinkedHashMap<>(cacheTemplate);
+					String id = "";
+					for (int j = 0; j < tr.children().size(); j++) {
+						List<String> keyList = new ArrayList<String>(cache.keySet());
+						Element td = tr.child(j);
+						String text = "";
+						if (j == 0) {
+							// 第一列有图标，处理
 						cache.put("IMG", td.select("a img").attr("src"));
 						id = td.select("a img").attr("alt");
 						text = td.ownText();
@@ -97,14 +89,25 @@ public class WikiaDataParse {
 				cache.put("STORE_ID", storeId);
 				ret.append(ParseUtil.buildSQL(cache, "TB_COMMERCIAL_MATERIAL", id));
 				ret.append("\n");
+			})	;
+
+				System.out.println("/**");
+				System.out.println(" * " + fileName.replace(".html", ""));
+				System.out.println(" */ ");
+				System.out.println(ret.toString());
+				System.out.println();
+
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
+
 		});
-		return ret.toString();
+
 	}
-	
+
 	public static void genFactoryMaterial() throws Exception {
 
-		File file = new File(PathUtil.getFileWorkPath("wikia", "factory_raw_material.html"));
+		File file = new File(PathUtil.getFileWorkPath("doc", "wikia", "factory_raw_material.html"));
 		Document document = Jsoup.parse(file, "GBK");
 		Elements ths = document.select("html body table thead tr th");
 		Map<String, Object> cacheTemplate = new LinkedHashMap<>();
@@ -112,14 +115,14 @@ public class WikiaDataParse {
 		ths.forEach(new Consumer<Element>() {
 			@Override
 			public void accept(Element t) {
-				if(colIndex.get() == 4) {
+				if (colIndex.get() == 4) {
 					return;
 				}
 				cacheTemplate.put(getThText(t), "");
 				colIndex.incrementAndGet();
 			}
 		});
-		
+
 		Elements trsInTBody = document.select("html body table tbody tr");
 		trsInTBody.forEach(new Consumer<Element>() {
 			@Override
@@ -128,21 +131,71 @@ public class WikiaDataParse {
 				List<String> keyList = new ArrayList<String>(cache.keySet());
 
 				String id = "";
-				for(int j = 0; j < tr.children().size() && j < colIndex.get(); j++) {
+				for (int j = 0; j < tr.children().size() && j < colIndex.get(); j++) {
 					Element td = tr.child(j);
-					if(j == 0) {
+					if (j == 0) {
 						// 第一列有图标，处理
 						cache.put("IMG", td.select("a img").attr("src"));
 						id = td.select("a img").attr("alt");
 					}
 					cache.put(keyList.get(j), td.ownText());
 				}
-				
+
 				System.out.println(ParseUtil.buildSQL(cache, "TB_FACTORY_MATERIAL", id));
 			}
 		});
 	}
-	
+
+	public static void genSpecialItems() throws Exception {
+
+		File file = new File(PathUtil.getFileWorkPath("doc", "wikia", "special_items.html"));
+		Document document = Jsoup.parse(file, "GBK");
+		// Special items doesn't have thead
+		Elements trs = document.select("html body table tbody tr");
+		Map<String, Object> cacheTemplate = new LinkedHashMap<>();
+		
+		// first raw is keys
+		Element trOfth = trs.get(0);
+		trOfth.children().forEach(th -> {
+			cacheTemplate.put(getThText(th), "");
+		});
+
+//		System.out.println(cacheTemplate);
+		
+		List<Element> trsOftd = trs.subList(1, trs.size());
+		trsOftd.forEach(tr -> {
+			
+			Elements tds = tr.select("td");
+			if(tds.size() != 0) {
+				StringBuilder ret = new StringBuilder();
+				Map<String, Object> cache = new LinkedHashMap<>(cacheTemplate);
+
+				String id = "";
+				for (int j = 0; j < tds.size(); j++) {
+					Element td = tds.get(j);
+					List<String> keyList = new ArrayList<String>(cache.keySet());
+					String text = "";
+					if (j == 0) {
+						// 第一列有图标，处理
+						cache.put("IMG", td.select("a img").attr("src"));
+						id = td.select("a img").attr("alt");
+						text = td.ownText();
+					} else if (j == 2) {
+						text = td.ownText().trim().replaceAll(",", "");
+					} else if (j == 3) {
+						text = getCondOfUnlocked(td);
+					} else {
+						text = td.ownText();
+					}
+					cache.put(keyList.get(j), text.replaceAll("'|’|‘", ""));
+				}
+				ret.append(ParseUtil.buildSQL(cache, "TB_SPECIAL_ITEMS", id));
+				System.out.println(ret);
+			}
+		});
+
+	}
+
 	/**
 	 * 获取节点的纯文本
 	 *
@@ -152,16 +205,37 @@ public class WikiaDataParse {
 		return ele.ownText().replaceAll(" ", "_").replaceAll("\\(", "").replaceAll("\\)", "").replaceAll("\\.", "").toUpperCase();
 	}
 	
+	/**
+	 * get the condition of unlocked
+	 *
+	 * @param td
+	 * @return
+	 */
+	private static String getCondOfUnlocked(Element td) {
+		String ownText = td.ownText().trim();
+		if(ownText.startsWith("Level")) {
+			return ownText.replaceAll(" ", "");
+		} else {
+			return "Population" + ownText.replaceAll("\"", "").replaceAll(",|\\.", "");
+		}
+	}
+
+	/**
+	 * get the mats of commercial material
+	 *
+	 * @param matsTd
+	 * @return
+	 */
 	private static String getMats(Element matsTd) {
-		String[] count = new String[]{};
-		if(matsTd.ownText() != null) {
+		String[] count = new String[] {};
+		if (matsTd.ownText() != null) {
 			count = matsTd.ownText().replaceAll("x", "*").split(" ");
 		}
 		StringBuilder sb = new StringBuilder();
 		Elements as = matsTd.select("a");
-		for(int i = 0; i < as.size(); i++) {
+		for (int i = 0; i < as.size(); i++) {
 			sb.append(as.get(i).select("img").attr("alt"));
-			if(i < count.length) {
+			if (i < count.length) {
 				sb.append(count[i]);
 			}
 			if (i != as.size() - 1) {
@@ -170,5 +244,5 @@ public class WikiaDataParse {
 		}
 		return sb.toString();
 	}
-	
+
 }
--- src/test/java/org/cnhalo/SqliteTest.java
+++ src/test/java/org/cnhalo/SqliteTest.java
@@ -20,7 +20,7 @@ import org.apache.commons.io.FileUtils;
  */
 public class SqliteTest extends TestCase {
 
-	public void testCommercialMaterial() {
+	public void testCommercialMaterials() {
 
 		try {
 			try (Connection conn = DriverManager.getConnection("jdbc:sqlite::memory:")) {
@@ -62,7 +62,7 @@ public class SqliteTest extends TestCase {
 
 	}
 	
-	public void testCommercialBuilding() {
+	public void testCommercialBuildings() {
 
 		try {
 			try (Connection conn = DriverManager.getConnection("jdbc:sqlite::memory:")) {
@@ -90,7 +90,7 @@ public class SqliteTest extends TestCase {
 
 	}
 	
-	public void testMaterialFactory() {
+	public void testFactoryMaterials() {
 
 		try {
 			try (Connection conn = DriverManager.getConnection("jdbc:sqlite::memory:")) {
@@ -129,6 +129,45 @@ public class SqliteTest extends TestCase {
 
 	}
 
+	public void testSpecialItems() {
+
+		try {
+			try (Connection conn = DriverManager.getConnection("jdbc:sqlite::memory:")) {
+				assertEquals(true, conn.isValid(1000));
+
+				File file = new File(getSqlFilePath("special_items.sql"));
+				assertEquals(true, file.exists());
+
+				String sqlContent = FileUtils.readFileToString(file);
+				try (Statement statement = conn.createStatement()) {
+					statement.executeUpdate(sqlContent);
+					
+					try (ResultSet rs = statement.executeQuery("select count(*) from TB_SPECIAL_ITEMS")) {
+						if (rs.next()) {
+							assertEquals(21, rs.getInt(1));
+						}
+					}
+					try (ResultSet rs = statement.executeQuery("select * from TB_SPECIAL_ITEMS")) {
+						String[] colums = new String[]{
+								"ID", "ITEM", "DESCRIPTION", "MAX_VALUE", "UNLOCKED_AT", "IMG"
+						};
+						if (rs.next()) {
+							// 测试一下列
+							ResultSetMetaData metaData = rs.getMetaData();
+							for (int i = 1; i < metaData.getColumnCount(); i++) {
+								assertEquals(colums[i - 1], metaData.getColumnName(i));
+							}
+						}
+					}
+				}
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
 	private static String getSqlFilePath(String fileName) {
 		return SqliteTest.class.getResource("/").getPath() + fileName;
 	}
